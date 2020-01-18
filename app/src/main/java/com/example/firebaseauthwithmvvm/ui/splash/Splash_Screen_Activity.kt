d98a3d2bebf5a78f15fc06745db251bde5754ce0
mac <eng.memohamed@gmail.com>
2020-01-18 23:26:44 +0200

    desgin of login, register and splash screen

--- app/src/main/java/com/example/firebaseauthwithmvvm/ui/splash/Splash_Screen_Activity.kt
+++ app/src/main/java/com/example/firebaseauthwithmvvm/ui/splash/Splash_Screen_Activity.kt
@@ -1,11 +1,18 @@
 package com.example.firebaseauthwithmvvm.ui.splash
 
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.AttributeSet
+import android.view.View
 import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.RelativeLayout
 import android.widget.VideoView
 import com.example.firebaseauthwithmvvm.R
 import com.example.firebaseauthwithmvvm.ui.auth.LoginActivity
@@ -13,6 +20,10 @@ import com.example.firebaseauthwithmvvm.ui.auth.LoginActivity
 class Splash_Screen_Activity : AppCompatActivity() {
 
     lateinit var videoView: VideoView
+    lateinit var imageView: ImageView
+    lateinit var rl_p: RelativeLayout
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,11 +34,37 @@ class Splash_Screen_Activity : AppCompatActivity() {
         )
 
 
-        setContentView(R.layout.activity_splash__screen_)
+        setContentView(R.layout.activity_splash_screen)
+
 
         supportActionBar?.hide()
 
+
         videoView = findViewById(R.id.videoView)
+
+        imageView = findViewById(R.id.civ_logo)
+
+        rl_p = findViewById(R.id.rl_p_splash_screen)
+
+
+        rl_p.startAnimation(AnimationUtils.loadAnimation(applicationContext, R.anim.fade_in))
+
+        rl_p.animation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationRepeat(p0: Animation?) {}
+            override fun onAnimationEnd(p0: Animation?) {
+                imageView.visibility = View.VISIBLE
+                imageView.startAnimation(AnimationUtils.loadAnimation(applicationContext, R.anim.fade_in))
+            }
+
+            override fun onAnimationStart(p0: Animation?) {}
+
+        })
+
+
+
+
+
+
 
         val video: Uri =
             Uri.parse("android.resource://" + packageName + "/" + R.raw.hazaz1_video_327)
@@ -40,6 +77,7 @@ class Splash_Screen_Activity : AppCompatActivity() {
             val test = Intent(this, LoginActivity::class.java)
             startActivity(test)
             finish()
-        }, 4000)
+        }, 9000)
     }
+
 }
